3b25f380c9d73fcd25387032f01bd42031c0ccb9
Harshita Dongre <harshitadongre@Harshitas-MacBook-Air.local>
2026-06-19 16:37:15 +0530

    Add entity implementations

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,32 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private String portfolioName;
+
+    protected Portfolio() {}
+
+    public Portfolio(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public String getPortfolioName() {
+        return portfolioName;
+    }
+
+    public void setPortfolioName(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+}
